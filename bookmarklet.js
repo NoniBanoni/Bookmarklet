b6646979864edcf867b098ec03d7bd34d23ebb49
NoniBanoni <41809798+NoniBanoni@users.noreply.github.com>
2019-05-17 12:06:51 -0700

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,10 +1,12 @@
-window.onbeforeunload = function() {
-  window.open(window.location.href).eval(`(${f})()`);
-  return""
-}
-window.onload = function() {
-  setTimeout(function() {
+(function f() {
+  window.onbeforeunload = function() {
     window.open(window.location.href).eval(`(${f})()`);
-  }, 1050)
-}
-window.onload()
+    return""
+  };
+  window.onload = function() {
+    setTimeout(function() {
+      window.open(window.location.href).eval(`(${f})()`);
+    }, 1050)
+  };
+  window.onload();
+})()
